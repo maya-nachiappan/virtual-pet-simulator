a2ae325c272a7675e03f0b7d2264e26692d4134b
maya-nachiappan <maya.n102304@gmail.com>
2024-11-16 20:09:33 -0800

    Initial Commit

---
+++ virtual pet simulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  virtual pet simulator
+//
+//  Created by maya nachiappan on 11/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
